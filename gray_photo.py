570df27c0171867e7fe2165e90df4881e0c2cc91
chihiro <chihiro.kiritake@gmail.com>
2023-04-01 21:39:18 +0900

    Add upload photo

--- gray_photo.py
+++ gray_photo.py
@@ -1,7 +1,22 @@
 import streamlit as st
 from PIL import Image
 
-with st.expander("白黒写真の撮影"):
+st.title("写真を白黒にするページ")
+
+uploaded_image = st.file_uploader("写真をアップロードする", type=['png','jpg'])
+
+if uploaded_image:
+
+    # Create a pillow image instance
+    img_upl = Image.open(uploaded_image)
+
+    # Convert the pillow image to grayscale
+    gray_img_upl = img_upl.convert("L")
+
+    # Render the grayscale image on the webpage
+    st.image(gray_img_upl)
+
+with st.expander("Webカメラで撮影した写真を白黒にする"):
 
     # Start the camera
     camera_image = st.camera_input("Take Photo を押してね。撮った写真を白黒にするよ。")
